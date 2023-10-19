d5e6f725116c718fbb25a9dbdaa7385a24b8ddc5
Alberto Alejandro Martinez Velazquez <avelazquez@abogadoericprice.com>
2023-10-19 08:56:27 -0700

    UPDATE: the new way date for this week, we need the other days to finish

--- src/pdf_site_traffic.php
+++ src/pdf_site_traffic.php
@@ -94,13 +94,13 @@ $week = [17, 18, 19, 20, 21, 22, 23];
 /* #endregion */
 /* #endregion */
 
-/* #region 10 October - 16 October*/
-$p_total_daily = [-32.56];
-$p_paid_search = [-81.13];
-$p_direct = [-14.29];
-$p_organic_search = [21.05];
-$p_organic_social = [-100];
-$p_referral = [0];
+/* #region 17 October - 23 October*/
+$p_total_daily = [-32.56, -23.53];
+$p_paid_search = [-81.13, -83.18];
+$p_direct = [-14.29, 21.95];
+$p_organic_search = [21.05, 21.05];
+$p_organic_social = [-100, -50];
+$p_referral = [0, 0];
 /* #endregion */
 
 /* #region FUNCTIONS */
