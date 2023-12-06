501b3d59ca5cf44463a4ff9ea2dab0c52bf4a19c
Avelazquez <avelazquez@abogadoericprice.com>
2023-12-05 17:18:43 -0800

    UPDATE: the database file for can try to create a better function and the report file now its update and have other data

--- src/database.php
+++ src/database.php
@@ -1,27 +1,23 @@
 <?php
 
-$date = date('Y-m-d H:i:s.uO');
+$myjson = <<<JSON
+[
+    {
+        "quote": "quote1",
+        "author": "aut1"
+    },
+    {
+        "quote": "quote2",
+        "author": "aut2"
+    }
+]
+JSON;
 
-create_month("test", $date);
+$json = "Current Json data";
+$json = json_decode($myjson);
+// $json = $json[0]->data;
 
-function create_month($name, $date)
-{
-    $host = "abogadoericprice.com";
-    $port = "5432";
-    $dbname = "dbezl1uquldojv";
-    $user = "uhgpgzxv2hhak";
-    $password = "700Flower!";
-
-    $connection_string = "host={$host} port={$port} dbname={$dbname} user={$user} password={$password} ";
-    $dbconn = pg_connect($connection_string) or die('Could not reach database.');
-
-    $sql = "INSERT INTO month_report(month_name, year_report, created_at)" .  "VALUES('" . cleanData($name) . "','" . cleanData($date) . "','" . cleanData($date) . "')";
-    return pg_affected_rows(pg_query($sql));
-}
-
-
-
-function cleanData($val)
-{
-    return pg_escape_string($val);
+foreach ($json as $key => $value) {
+    echo $value;
+    echo "<br/>";
 }
